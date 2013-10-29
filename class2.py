2976d6f6c734376d625d55f25a71e78d890cb5e8
Lando21 <Lando@Landons-MacBook-Pro.local>
2013-10-29 10:01:03 -0700

    worked with funcitons

---
+++ class2.py
@@ -0,0 +1,15 @@
+def f1():
+    print "hello1"
+
+
+def f2(name):
+    print name
+    print type (name)
+    name = "oh no"
+
+
+
+my_name = "Lando"
+print f2(my_name)
+
+print my_name
---
+++ day1.py
@@ -0,0 +1,17 @@
+my_switch = True
+if my_switch == 1:
+    print " I am true"
+elif my_switch == 2:
+    print "hello"
+else:
+    print "hhhhh"
+
+
+
+
+
+
+
+
+
+print "HI"
